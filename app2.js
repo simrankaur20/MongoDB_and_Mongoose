5fbba63ab34c7c43de22c4c841e64c6784eedf69
unknown <simrankr361@gmail.com>
2020-01-01 20:25:56 +0530

    add mongoose code commit2

--- app2.js
+++ app2.js
@@ -7,8 +7,15 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB",{ useNewUrlParser: true ,u
 
 //define a schema(structure) that every fruit object would have
 const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+       // required: [true,"Please mention name"] // name is compulsory field
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10    //validation in mongoose
+    },
     review: String
 }) ;
  //in mongoose specify a singular name for your collection
@@ -19,9 +26,9 @@ const fruitSchema = new mongoose.Schema({
  //now,you can create a new fruit document(from the model fruitschema)
 
  const fruit = new Fruit ({
-     name: "Apple",
-     rating: 7,
-     review: "Pretty solid as a fruit"
+    // name: "Apple",
+     rating: 4,
+     review: "Peaches are great"
  })
 
  //fruit.save();
@@ -30,14 +37,24 @@ const fruitSchema = new mongoose.Schema({
 
  const PersonSchema = new mongoose.Schema({
     name: String,
-    Age: Number
+    Age: Number,
+    favoriteFruit: fruitSchema
  });
 
  const Person = mongoose.model("Person",PersonSchema);
+ 
+ const strawberry = new Fruit({
+    name: "strawberry",
+    rating: 9,
+    review: "great"
+ });
+
+ strawberry.save(); 
 
  const person = new Person({
-    name: "John",
-    Age: 42
+    name: "john",
+    Age: 12,
+    favoriteFruit: strawberry
  })
 
  person.save();
@@ -86,3 +103,31 @@ Fruit.find(function(err,fruits){
     }
 });
 //fruits is an array of javascript object;
+
+
+Fruit.updateOne({_id: "5e0caa46df66df153854ed6e"}, {name: "Peach"}, function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("success update");
+    }
+})
+
+Fruit.deleteOne({_id:"5e0cab8e47fedf2b60ed5ca8"}, function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("deleted");
+    }
+})
+
+Person.deleteMany({name: "John"}, function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("deleted Johns");
+    }
+})
